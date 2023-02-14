0a71712a2af7dcf8d04b9d03adce7c62814cd387
Ivan Ega Pratama <ivan.ega.p@gmail.com>
2023-02-14 11:33:41 +0700

    add histoire

---
+++ src/lib/components/design-system/Card.story.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import '../../../app.css';
+	import Card from './Card.svelte';
+
+	export let Hst: Hst;
+	let content: string = 'Hello';
+</script>
+
+<Hst.Story>
+	<Card {content} />
+
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={content} title="Content" />
+	</svelte:fragment>
+</Hst.Story>
